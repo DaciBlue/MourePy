cfe420fb799875d76582eb496cf2b47537eb7cdd
DaciBlue <150244411+DaciBlue@users.noreply.github.com>
2024-11-15 11:53:55 +0100

    primera leccion hola mundo

---
+++ hello.py
@@ -0,0 +1,24 @@
+# Esto es un comentario: Hola mundo
+# El hola mundo en Python
+print("Hola Python")
+print('Hola Python')
+
+# Consultar el tipo de dato
+print(type("Soy un dato string")) # Tipo string
+print(type(5)) # Tipo integer
+print(type(1.5)) # Tipo float
+print(type(3 + 1j)) # Tipo compljeo
+print(type(True)) # Tipo booleano
+
+
+"""
+Este es un
+comentario
+en varias líneas
+"""
+
+'''
+Este tambien
+es un comentario
+en varias líneas
+'''
